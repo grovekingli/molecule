6eccf8e96f53406b5a8cb69c10d3e56d88fbd39f
price <lixingyu_price@163.com>
2020-09-02 21:56:15 +0800

    mode demo file

--- inheritance/type2.js
+++ inheritance/type2.js
@@ -1,7 +1,11 @@
+// 借用构造函数继承
+
+// 定义Person类
 function Person(name) {
     this.name = name;
 }
 
+// 定义student类
 function Student(name, grade) {
     Person.call(this, name)
     this.grade = grade;
--- inheritance/type3.js
+++ inheritance/type3.js
@@ -1,7 +1,11 @@
+// 组合式继承
+
+// 定义Person类
 function Person(name) {
     this.name = name;
 }
 
+// 定义student类
 function Student(name, grade) {
     Person.call(this, name)
     this.grade = grade;
