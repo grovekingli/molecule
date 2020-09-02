eb992d7fcbd147ccf564a652b66c6127c48008eb
price <lixingyu_price@163.com>
2020-09-02 22:41:49 +0800

    modify the def of class in inheritance demo file

--- inheritance/type2.js
+++ inheritance/type2.js
@@ -3,7 +3,17 @@
 // 定义Person类
 function Person(name) {
     this.name = name;
+    this.setName = function(name){
+        this.name = name;
+    };
+    this.getName = function() {
+        return this.name;
+    };
 }
+
+Person.prototype.sayName = function() {
+    console.log('My name is ' + this.getName() + '.');
+};
 
 // 定义student类
 function Student(name, grade) {
@@ -14,3 +24,4 @@ function Student(name, grade) {
 var studentObj = new Student('lxy',1);
 
 console.log(studentObj.name, studentObj.grade);
+Student.sayName();
--- inheritance/type3.js
+++ inheritance/type3.js
@@ -3,12 +3,27 @@
 // 定义Person类
 function Person(name) {
     this.name = name;
+    this.setName = function(name){
+        this.name = name;
+    }
+    this.getName = function() {
+        return this.name;
+    };
 }
+
+Person.prototype.sayName = function() {
+    console.log('My name is ' + this.getName() + '.');
+};
 
 // 定义student类
 function Student(name, grade) {
     Person.call(this, name)
     this.grade = grade;
 }
+Student.prototype = new Person();
 
-//TODO...
+var studentObj = new Student('lxy', 1);
+
+console.log(studentObj.name, studentObj.grade);
+studentObj.setName('lxx');
+studentObj.sayName();
